be2bbce5ef83002a7f5afa5e8f1d818ad475c12e
Adrien <adrien.designs@gmail.com>
2019-10-17 01:29:38 -0400

    done!

--- js/index.js
+++ js/index.js
@@ -1,49 +1,72 @@
 // VARIABLES
 
 let subject = {
-  word: "watashi",
-  meaning: "I",
+  word: 'watashi',
+  meaning: 'I',
+  particle: 'wa',
 };
-let verb = {
-  word: "tabe",
-  meaning: "eat",
-  endingsAlt: {
-    presentTensePolite: "masu",
-    pastTensePolite: "mashita",
-    presentTenseCasual: "ru",
-    pastTenseCasual: "ta",
-  },
-  endings: {
-    present: {
-      polite: "masu",
-      casual: "ru",
+
+let object = {
+  word: 'mango',
+  meaning: 'mango',
+  particle: 'wo',
+};
+
+let endings = {
+  'endings': {
+    'present': {
+      'polite': 'desu',
+      'casual': '',
     },
-    past: {
-      polite: "mashita",
-      casual: "ta",
+    'past': {
+      'polite': 'deshita',
+      'casual': 'datta',
+    }  
+  }
+};
+
+let verb = {
+  'word': 'tabe',
+  'meaning': {
+    'present': 'eat',
+    'past':'ate',
+  },
+  'endings': {
+    'present': {
+      'polite': 'masu',
+      'casual': 'ru',
+  },
+    'past': {
+      'polite': 'mashita',
+      'casual': 'ta',
     }
   }
 };
-// let object = {
-//   meaning: "mango",
-//   word: "mango",
-//   };
-// let subject;
-// let subjectEng;
-let object;
-let objectEng;
+
+
+let tense = 'present';
+let tone = 'polite';
+
+
+let destination = {
+  word: 'uchi',
+  meaning: 'at home',
+  particle: 'ni',
+};
+
+let time = {
+  word: 'kyo',
+  meaning: 'Today',
+};
+
 let subjectUsage = false;
 let objectUsage = false;
-// let verb;
 let verbUsage = false;
-let verbEnding;
 let endingUsage = false;
-let makeIt1 = "present";
-let makeIt2 = "casual";
 
-// $('.add-topic').on('click', clickTopic);
-
-
+if (subjectUsage === false && objectUsage === false && verbUsage === false) {
+  $('.add-make-it, .reset').addClass('disabled');
+};
 
 // ON CLICK EVENTS
 
@@ -54,134 +77,187 @@ $('.add-object').on('click', clickObject);
 
 $('.add-verb').on('click', clickVerb);
 
-$('.add-location').on('click', clickLocation).attr("disabled", true);
+$('.add-location').on('click', clickLocation);
 
-$('.add-time').on('click', clickLocation).attr("disabled", true);
+$('.add-time').on('click', clickTime);
 
 $('.add-make-it').on('click', clickMakeIt);
 
+$('.reset').on('click', clickReset);
 
 
-// function clickTopic(watashi) {
-//   watashi = "watashi " + "wa";
-//   $('.topic').text(watashi);
-// }
 
 // FUNCTIONS
 
 function clickSubject() {
   subjectUsage = true;
-  // subject = 'watashi';
-  // subjectEng = 'I';
-
   $('.subject').text(subject.word);
   $('.eng-subject').text(subject.meaning);
 
-  if (objectUsage === true) {
-    $('.subject-particle').text('wa');
-    $('.eng-subject').append(' am a ')
-  } else {  
-    $('.ending-particle').text('desu');
+  if (objectUsage === true && verbUsage === true) {
+    $('.subject-particle').text(subject.particle);
+    $('.eng-subject').text('I');
+
+  } else if (objectUsage === true) {
+    $('.subject-particle').text(subject.particle);
+    $('.eng-subject').text('I am a ')
+
+  } else if (verbUsage === true) {
+    $('.subject-particle').text(particle.subject);
+    $('.ending-particle').text(endings.endings[tense][tone]);
+    $('.eng-subject').text('I');
+
+  } else {
     $('.eng-subject').prepend('(It is) ');
-  }
+    $('.ending-particle').text(endings.endings[tense][tone]);
+  };
 
-  $('.add-subject').attr("disabled", true);
+  $('.add-subject').addClass('disabled');
+  $('.add-make-it, .reset').removeClass('disabled');
 
-}
+};
 
 function clickObject() {
   objectUsage = true;
-  object = 'mango';
-  objectEng = 'mango';
 
-  $('.object').text(object);
-  $('.eng-object').text(objectEng);
+  $('.object').text(object.word);
+  $('.eng-object').text(object.meaning);
 
 
-  if (subjectUsage === true) {
-    $('.subject-particle').text('wa');
-    $('.eng-subject').text('I am a ');
-  
-  } else if (verbUsage === true) {
-    $('.object-particle').text('wo');
-    $('.eng-subject').prepend('(I) ');
+  if (subjectUsage === true && verbUsage === true) {
+    $('.subject-particle').text(subject.particle);
+    $('.object-particle').text(object.particle);
+    $('.eng-subject').text('I ');
+
+  } else if (subjectUsage === true) {
+    $('.subject-particle').text(subject.particle);
+    $('.eng-subject').text('I am ');
+    $('.ending-particle').text(endings.endings[tense][tone]);
+
+  } else if (verbUsage === true){
+    $('.object-particle').text(object.particle);
 
   } else {
-    $('.ending-particle').text('desu');
-    $('.eng-subject').prepend('It is ');
-  }
+    $('.ending-particle').text(endings.endings[tense][tone]);
+    $('.eng-subject').text('(I am) ');
+  };
 
-  $('.add-object').attr("disabled", true);
-}
 
-function clickLocation() {
-  let location = "kissaten";
-  $('.location').text(location);
+  $('.add-object').addClass('disabled');
+  $('.add-make-it, .reset').removeClass('disabled');
 
-  let locationParticle = "ni";
-  $('.location-particle').text('ni');
+};
 
-}
 
 function clickVerb() {
   verbUsage = true;
-  // verbEnding = 'masu';
-  // verb = 'tabe';
-  // verbEng = 'eat';
 
-  $('.verb').text(verb.word + verb.endings[makeIt]);
-  $('.eng-verb').text(verb.meaning);
+  $('.verb').text(verb.word + verb.endings[tense][tone]);
 
-  if (subjectUsage === true){
+  $('.eng-verb').text(verb.meaning[tense]);
+
+
+  if (subjectUsage === true && objectUsage === true) {
     $('.eng-subject').text('I');
+    $('.object-particle').text(object.particle);
+    $('.ending-particle').remove();
+
+  } else if (objectUsage === true) {
+    $('.eng-subject').text('(I)');
+    $('.object-particle').text(object.particle);
     $('.ending-particle').text('');
 
-  }
-$('.add-verb').attr("disabled", true);
+  } else if (subjectUsage === true){
+    $('.eng-subject').text('I');
+    $('.subject-particle').text(subject.particle);
+    $('.ending-particle').text('');
 
-}
+  } else {
+    $('.eng-subject').text('(I)');
+    $('.ending-particle').text('');
+  };
+
+
+  $('.add-verb').addClass('disabled');
+  $('.add-make-it, .reset').removeClass('disabled');
+
+};
+
+function clickLocation() {
+
+  $('.location').text(destination.word);
+  $('.location-particle').text(destination.particle);
+  $('.eng-location').text(destination.meaning);
+
+  $('.add-location').addClass('disabled');
+  $('.reset').removeClass('disabled');
+
+  if (subjectUsage === true){
+    $('.subject-particle').text(subject.particle);
+  };
+
+};
+
+function clickTime() {
+
+  $('.time').text(time.word);
+  $('.eng-time').text(time.meaning);
+  $('.add-time').addClass('disabled');
+  $('.reset').removeClass('disabled');
+
+};
+
 
 function clickMakeIt() {
-  // $('#idCheckbox').prop('checked', true);
 
-  console.log($(this).val(), $(this).prop('checked'));
-
-  // create a condition that checks each checkbox for true or false (look into $('element').prop('checked'))
-  // for each of them we need to set if makeIt1 = past or present and makeIt2 = polite or casual
-
-  // let makeIt = $(this).val();
-  $('.verb').text(verb.word + verb.endings[makeIt1][makeIt2]);
+  if ($('.tense').prop('checked')) {
+    tense = 'past';
+    $('.ten').find('span').text('Present')
 
 
-  // if (makeIt = presentTensePolite){ 
-  //   $('.add-make-it').val(pastTensePolite);
-  //   console.log($('.add-make-it').val())
+  } else {
+    tense = 'present';
+    $('.ten').find('span').text('Past')
 
-  // } else if (makeIt = pastTenseCasual) {
-  //   $('.add-make-it').val(presentTenseCasual);
+  };
+   // tense = ($('.tense').prop('checked')) ? 'past' : 'present';
 
-  // } else if (makeIt = casual) {
-  //   $('.add-make-it').val(formal);
+  if ($('.tone').prop('checked')) {
+    tone = 'casual';
+    $('.ton').find('span').text('Polite')
 
-  // } else if (makeIt = formal) {
-  //   $('.add-make-it').val(casual);
+  } else {
+    tone = 'polite';
+    $('.ton').find('span').text('Casual')
 
-  // }
+  };
 
-  // verb.endings[presentVar][politeVar]
+  if (verbUsage) {
+    $('.verb').text(verb.word + verb.endings[tense][tone]);
+    $('.eng-verb').text(verb.meaning[tense]);
 
-  
+  } else if (subjectUsage || objectUsage) {
+    $('.ending-particle').text(endings.endings[tense][tone]);
 
-}
+  };
+};
 
-// if
+function clickReset() {
 
-if (verbUsage === true && objectUsage === true && verbUsage === true){
-  $('.add-location').removeAttr("disabled");
-  $('.add-time').removeAttr("disabled");
-}
+  subjectUsage = false;
+  objectUsage = false;
+  verbUsage = false;
+  endingUsage = false;
+
+  $('.add-subject, .add-object, .add-verb, .add-location, .add-time').removeClass('disabled');
+  $('.add-make-it, .reset').addClass('disabled');
+  $(this).prev().children('span').text('');
+
+  $('.eng-time, .eng-subject, .eng-verb, .eng-object, .eng-location').text('');
+
+};
 
 
 
- 
+
 
